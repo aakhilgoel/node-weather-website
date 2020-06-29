637f179a3782585baf829dbc9345159fe2a079d4
Goel <akhil.goel@newyork.hbpub.net>
2020-06-28 22:54:55 -0400

    added few fields in forecase.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.location.name === null) {
             callback('Please specify the correct location', undefined);
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}, It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out`)
+            callback(undefined, `${body.current.weather_descriptions[0]}, It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.
+            Humidity currently is ${body.current.humidity}. Current visiblity is ${body.current.visibility}`)
         }
     });
 }
